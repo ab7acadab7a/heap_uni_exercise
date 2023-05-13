4bec5a2b1f6b5d47c01a3af17a0af650d98f416d
tp <tompppp@gmail.com>
2023-05-13 14:41:14 +0300

    finally finished

--- src/main.py
+++ src/main.py
@@ -1,29 +1,10 @@
-from src.gui.console_handler import ConsoleHandler
-from src.heap.heap import Heap
+from src.console_handler import ConsoleHandler
 
-ch = ConsoleHandler('./configuration/data.json')
-ch.load_json_configuration('./configuration/data.json')
-heap = Heap(ch.heap.heap)
+# fill if there are errors with the dir (default should be automatic for your correct path)
+root_path = ""
 
-heap.print_heap()
-heap.heap_extract_max()
-heap.heap_extract_min()
 
-heap.print_heap()
-print("adding 9")
-heap.heap_insert(9)
+if __name__ == '__main__':
+    ch = ConsoleHandler(root_path)
+    ch.run()
 
-heap.print_heap()
-print("adding 1")
-heap.heap_insert(1)
-
-heap.print_heap()
-ch.load_json_configuration('./configuration/data2.json')
-heap2 = Heap(ch.heap.heap)
-ConsoleHandler('./configuration/data2.json')
-print("adding 9")
-heap2.heap_insert(9)
-heap.print_heap()
-print("removing 2")
-heap2.heap_delete(2)
-heap2.print_heap()
