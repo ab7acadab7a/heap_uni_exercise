3469b12a70185e825a007fed764b02529b14f933
tp <tompppp@gmail.com>
2023-05-13 13:10:05 +0300

    added some edge cases

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from src.gui.console_handler import ConsoleHandler
 
 ch = ConsoleHandler('./configuration/data.json')
 
-ch.heap.heapify(0)
 ch.heap.print_heap()
 ch.heap.heap_extract_max()
 ch.heap.heap_extract_min()
@@ -15,4 +14,12 @@ ch.heap.print_heap()
 print("adding 1")
 ch.heap.heap_insert(1)
 
+ch.heap.print_heap()
+
+ch = ConsoleHandler('./configuration/data2.json')
+print("adding 9")
+ch.heap.heap_insert(9)
+ch.heap.print_heap()
+print("removing 2")
+ch.heap.heap_delete(2)
 ch.heap.print_heap()
